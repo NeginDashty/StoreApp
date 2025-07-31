bd2dd721f66f6cb9a05791cb685207cbb810c834
neginDashty <negindashti.pilehroud@gmail.com>
2025-07-31 16:07:48 +0330

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import ProductsPage from "./Pages/ProductsPage";
+import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <nav>
+    <Link to="/products">برو به محصولات</Link>
+  </nav>
+ 
+  <Routes>
+    <Route path="/products" element={<ProductsPage/>}/>
+  </Routes>
+  </>
   );
 }
 
